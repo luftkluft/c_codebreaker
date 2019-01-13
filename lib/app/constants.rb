26039003bbb9a3599b3a0c48d5358be800b271c9
luft kluft <luftkluft@gmail.com>
2019-01-13 09:11:05 +0300

    Feature/dev: 1.8.1 constants

--- lib/app/constants.rb
+++ lib/app/constants.rb
@@ -3,3 +3,8 @@
 WEB = 'web'
 CONSOLE = 'console'
 NUMBER_OF_DIJITS = 4
+TEST_PATH = 'database/test.yml'.freeze
+SCORE_DATABASE = 'database/data.yml'.freeze
+TEST_LEVEL = 'hell'.freeze
+TEST_NAME = 'Name'.freeze
+TEST_NUMBER = '1234'.freeze
