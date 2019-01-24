738b1a7fa94d1c612941d7558bd859fdc1384b13
luft kluft <luftkluft@gmail.com>
2019-01-25 00:09:48 +0300

    Feature/dev: uploader class

---
+++ lib/app/entities/game.rb
@@ -0,0 +1,16 @@
+class Game
+  DIFFICULTIES = {
+    easy: {
+      attempts: 15,
+      hints: 2
+    },
+    medium: {
+      attempts: 10,
+      hints: 1
+    },
+    hell: {
+      attempts: 5,
+      hints: 1
+    }
+  }.freeze
+end
---
+++ lib/app/entities/uploader.rb
@@ -0,0 +1,25 @@
+class Uploader
+  FILE_NAME = 'database/data.yml'.freeze
+
+  def create
+    File.new(FILE_NAME, 'w')
+    File.write(FILE_NAME, [].to_yaml)
+  end
+
+  def load
+    YAML.load_file(File.open(FILE_NAME)) if storage_exist?
+  end
+
+  def save(object)
+    File.open(FILE_NAME, 'w') { |file| file.write(YAML.dump(object)) }
+  end
+
+  def storage_exist?
+    File.exist?(FILE_NAME)
+  end
+
+  def save_game_result(object)
+    create unless storage_exist?
+    save(load.push(object))
+  end
+end
